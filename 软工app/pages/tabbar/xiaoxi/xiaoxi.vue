590a7130460e8c907658b22fea1dc8a2471ba22b
zaohuan <1870471060@qq.com>
2024-10-03 11:32:30 +0800

    Update xiaoxi.vue

--- 软工app/pages/tabbar/xiaoxi/xiaoxi.vue
+++ 软工app/pages/tabbar/xiaoxi/xiaoxi.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-		
+		<view class="box1">
+			<text class="biaoti">
+				消息
+			</text>
+			<image style=" width: 27px;height: 27px;padding-left: 238px;padding-top: 50px;;" src="../../../static/sandian.png">
+				
+			</image>
+		</view>
 	</view>
 </template>
 
@@ -18,5 +25,18 @@
 </script>
 
 <style>
-
+	.box1{
+		width: 100%;
+		height: 50px;
+		background: #FFFFFF;
+		
+		padding-left: 30px;
+		padding-top: 20px;
+		padding-bottom:60px;
+		
+		
+	}
+	.biaoti{
+		font-size: 55rpx;
+	}
 </style>
