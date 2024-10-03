e7155102870a49bb57671755d11d262973725ce2
zaohuan <1870471060@qq.com>
2024-10-03 11:31:04 +0800

    Update lianxiren.vue

--- 软工app/pages/tabbar/lianxiren/lianxiren.vue
+++ 软工app/pages/tabbar/lianxiren/lianxiren.vue
@@ -1,6 +1,14 @@
 <template>
 	<view>
-		
+		<view class="box1">
+			<text class="biaoti">
+				联系人
+			</text>
+			<image style=" width: 27px;height: 27px;padding-left: 210px;padding-top: 50px;;" src="../../../static/sandian.png">
+				
+			</image>
+		</view>
+
 	</view>
 </template>
 
@@ -18,5 +26,18 @@
 </script>
 
 <style>
-
+	.box1{
+		width: 100%;
+		height: 50px;
+		background: #FFFFFF;
+		
+		padding-left: 30px;
+		padding-top: 20px;
+		padding-bottom:60px;
+		
+		
+	}
+	.biaoti{
+		font-size: 55rpx;
+	}
 </style>
