6296b8e5a8c52a007e86195fc920eadfa71b8e40
hsf <2796608260@qq.com>
2024-10-03 15:39:48 +0800

    Update shouye.vue

--- 软工app/pages/tabbar/shouye/shouye.vue
+++ 软工app/pages/tabbar/shouye/shouye.vue
@@ -7,9 +7,28 @@
 			
 		</view>
 	</view>
+	
+	<view>
+		<uni-section>
+			<uni-search-bar class="uni-mt-10" radius="5" placeholder="搜索人/群/项目" clearButton="auto" cancelButton="none" @confirm="search" />
+		</uni-section>
+		<Daohang />
+	</view>
+	
+	<view class ="tuijian">
+		<view class="tuijian_box tuijian_box2">
+			<view class="tuijian_T">
+				推荐项目
+			</view>
+		</view>
+		<Tuijian />
+	</view>
+	
 </template>
 
 <script>
+	import Daohang from "../../../uni_modules/daohang/daohang.vue"
+	import Tuijian from "../../../uni_modules/tuijian/tuijian.vue"
 	export default {
 		data() {
 			return {
@@ -18,6 +37,10 @@
 		},
 		methods: {
 			
+		},
+		components:{
+			Daohang,
+			Tuijian
 		}
 	}
 </script>
@@ -37,4 +60,23 @@
 	.biaoti{
 		font-size: 55rpx;
 	}
+	.tuijian_box2{
+		display:flex;
+		width:100%;
+		justify-content: center;
+		align-items: center;
+		box-sizing: border-box;
+		overflow: hidden;
+		padding:0 15px;
+		justify-content: space-between;
+		align-content: space-between;
+		flex-wrap: wrap;
+		
+	}
+		
+	.tuijian_T{
+		font-size: 20px;
+		font-weight: 700;
+	}
 </style>
+
