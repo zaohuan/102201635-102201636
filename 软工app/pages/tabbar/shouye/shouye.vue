6c03388094b62833e4ea18ae3713b8f36eb93497
zaohuan <1870471060@qq.com>
2024-10-05 19:35:44 +0800

    Update shouye.vue

--- 软工app/pages/tabbar/shouye/shouye.vue
+++ 软工app/pages/tabbar/shouye/shouye.vue
@@ -3,7 +3,17 @@
         <view>
             <view class="box1">
                 <text class="biaoti">首页</text>
+				<image class="jia" src="../../../static/jia.png" @tap="toggleView">
+				</image>
             </view>
+			<cover-view v-if="showView" style="z-index: 999; position: fixed; top: 0; left: 0; right: 0; bottom: 0; background-color: rgba(0, 0, 0, 0.5); display: flex; justify-content: center; align-items: center;">
+			  <!-- 内容区域 -->
+			  <view style="align-content: center; display: flex; flex-direction: column; width: 80%; height: 200px; background-color: #fff; padding: 20px; border-radius: 10px;">
+			    <text class="chuang" @click="chuang">创建项目</text>
+				<text class="jiaru">加入项目</text>
+			    <button @tap="toggleView" style="margin-top: 40px;">关闭</button>
+			  </view>
+			</cover-view>
             <uni-search-bar
                 class="uni-mt-10"
                 radius="5"
@@ -32,10 +42,20 @@ export default {
     data() {
         return {
             searchKeyword: '', 
+			showView: false
         };
     },
     methods: {
+		toggleView() {
+		     this.showView = !this.showView;
+		   },
+		   chuang(){
+		   	uni.navigateTo({
+		   		url:"/pages/chuangjianxiangmu/chuangjianxiangmu"
+		   	})
+		   },
         search() {
+			
             if (this.searchKeyword.trim()) {
                 uni.navigateTo({
                     url: `/pages/sousuo/sousuo?keyword=${encodeURIComponent(this.searchKeyword)}` 
@@ -88,4 +108,20 @@ export default {
         padding-top: 60px;
         padding-bottom: 25px;
     }
+	.jia{
+		width: 23px;
+		height: 23px;
+		padding-left: 245px;
+		padding-top: 70px;
+	}
+	.chuang{
+		font-size: 25px;
+		padding-left: 105px;
+		padding-top:15px;
+	}
+	.jiaru{
+		font-size: 25px;
+		padding-left: 105px;
+		padding-top:20px;
+	}
 </style>
