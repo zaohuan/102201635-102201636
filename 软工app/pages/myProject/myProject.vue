c368231bf815ca6f9483494b5560879e1db3f98c
zaohuan <1870471060@qq.com>
2024-10-07 16:44:37 +0800

    Update myProject.vue

--- 软工app/pages/myProject/myProject.vue
+++ 软工app/pages/myProject/myProject.vue
@@ -8,18 +8,31 @@
 				返回
 			</text>
 		</view>
-		<uni-collapse>
+		<uni-collapse v-if="createdProjects.length > 0">
 		    <uni-collapse-item :show-animation="true" title="我创建的项目">
-		        <uni-list :border="true">
-		           
+		        <uni-list :border="true" v-if="createdProjects.length > 0">
+		            <uni-list-item 
+		                v-for="(project, index) in createdProjects" 
+		                :key="index"
+		                :title="project.data.name" 
+		                :note="project.data.description" 
+						clickable="true" 
+						@click="navigateToDetails(project._id)">
+		            </uni-list-item>
 		        </uni-list>
 		    </uni-collapse-item>
 		
-		    <uni-collapse-item :show-animation="true" title="我加入的项目">
+		   <!-- <uni-collapse-item :show-animation="true" title="我加入的项目">
 		        <uni-list :border="true">
-		            
+		            <uni-list-item 
+		                v-for="(project, index) in joinedProjects" 
+		                :key="index" 
+		                :title="project.projectName" 
+		                :note="project.projectDetails"
+		                clickable="true">
+		            </uni-list-item>
 		        </uni-list>
-		    </uni-collapse-item>
+		    </uni-collapse-item> -->
 		</uni-collapse>
 		
 	</view>	
@@ -35,18 +48,70 @@
 <script>
 export default {
   data() {
-    return {
-     
-    };
+      return {
+          createdProjects: [], // 存储我创建的项目
+          joinedProjects: [] // 存储我加入的项目
+      };
   },
  
-  methods:{
-	  fanhui()
-	  {
-	  	uni.switchTab({
-	  		url:"/pages/tabbar/wode/wode"
-	  	})
-	  }
+  mounted() {
+      
+  },
+  onShow(){
+	  this.getProjects();
+  },
+  methods: {
+		  async getProjects() {
+		      const db = uniCloud.database();
+		      
+		      
+		      const usernameRes = await uni.getStorage({
+		          key: 'username'
+		      });
+		      const username = usernameRes.data; 
+		      
+		      console.log('查询时的用户名', username); 
+		  
+		      
+		  
+		      //确保用户名不为空
+		      if (username) {
+		          // 查询我创建的项目
+		          const createdRes = await db.collection('projects')
+		              .where({
+		                  'data.username': username // 使用转换为字符串的 username
+		              })
+		              .get();
+		          
+		          // 把查询到的项目数据赋值给 createdProjects
+		          this.createdProjects = createdRes.result.data;
+		          
+		          // 输出查询到的项目数据，检查是否获取到项目
+		          console.log('查询到的项目:', this.createdProjects);
+		      } else {
+		          console.error('未获取到用户名');
+		      }
+		  },
+  
+          // // 假设加入的项目通过项目表中的 "members" 字段存储成员信息
+          // const joinedRes = await db.collection('projects')
+          //     .where({
+          //         members: userID // 根据成员列表中的 userID 查询加入的项目
+          //     })
+          //     .get();
+          // this.joinedProjects = joinedRes.result.data;
+      
+		navigateToDetails(projectId) {
+		      uni.navigateTo({
+		        url: `/pages/projectDetails/projectDetails?id=${projectId}` // 传递项目的 id
+		      });
+		    },
+      // 返回按钮
+      fanhui() {
+          uni.switchTab({
+              url: "/pages/tabbar/wode/wode"
+          });
+      }
   }
 };
 </script>
