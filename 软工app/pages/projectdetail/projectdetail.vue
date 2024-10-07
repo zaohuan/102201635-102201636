f6f8140fb2bb1a9f5c7685ae20084ad769d9e4ba
hsf <2796608260@qq.com>
2024-10-07 20:09:39 +0800

    Update projectdetail.vue

--- 软工app/pages/projectdetail/projectdetail.vue
+++ 软工app/pages/projectdetail/projectdetail.vue
@@ -1,16 +1,14 @@
 <template>
 	<view class="container">
-		<view class="box1">
-			<text class="biaoti">
-				项目详情
-			</text>	
-			<text class="fanhui" @click="fanhui">
-				返回
-			</text>
+		<view class="box3">
+			<view class="back-btn" @click="fanhui">
+				<text class="back-icon">返回</text>
+			</view>
+			<text class="biaoti2">项目详情</text>
 		</view>
 		
-		<view class="details" v-if="projectData">
-			<uni-section class="mb-10" title="基础用法" sub-title="副标题"></uni-section>
+		<view class="detailssss" v-if="projectData">
+
 			<text>项目名称：{{ projectData.name }}</text>
 			<text>项目简介：{{ projectData.description }}</text>
 			<text>项目分类：{{ projectData.category }}</text>
@@ -67,31 +65,35 @@ export default {
 		display: flex;
 		flex-direction: column;
 	}
-.box1{
-		width: 100%;
-		height: 120px;
-		background: #FFFFFF;
-		display: flex;
-		
-		
-		
-	}
-	
-.biaoti{
-		font-size: 55rpx;
-		font-weight: 700;
-		padding-left: 25px;
-		padding-top: 60px;
-		padding-bottom:25px;
-	}
-.details{
-	margin-top: 20px;
+.box3{
+    width: 100%;
+    height: 80px;
+    background: #FFFFFF;
+    display: flex;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+    justify-content: center;
+    align-items: center;
+}
+.biaoti2 {
+  font-size: 55rpx;
+  font-weight: 700;
+}
+.back-btn {
+  position: absolute;
+  left: 15px;
+  display: flex;
+  align-items: center;
+}
+.back-icon {
+  font-size: 35rpx;
+  color: #000000;
+}
+.detailssss{
+	word-wrap: break-word;
+	margin-top: 10%;
+	margin-left: 5%;
 	display: flex;
+	font-size: 20px;
 	flex-direction: column;
 }
-.fanhui{
-		font-size: 20px;
-		padding-top: 70px;
-		padding-left: 190px;
-	}
 </style>
