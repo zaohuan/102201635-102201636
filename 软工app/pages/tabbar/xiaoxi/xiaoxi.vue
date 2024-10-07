eb4bace186dd4641300a11ca1c4a1f4fb2b87fde
hsf <2796608260@qq.com>
2024-10-07 14:45:21 +0800

    Update xiaoxi.vue

--- 软工app/pages/tabbar/xiaoxi/xiaoxi.vue
+++ 软工app/pages/tabbar/xiaoxi/xiaoxi.vue
@@ -1,50 +1,49 @@
 <template>
-	<view>
-		<view class="box1">
-			<text class="biaoti">
-				消息
-			</text>
-			<image class="sandian"  src="../../../static/sandian.png">
-				
-			</image>
-		</view>
-	</view>
+    <view>
+        <view class="box1">
+            <text class="biaoti">
+                消息
+            </text>
+            <image class="sandian"  src="../../../static/sandian.png">
+                
+            </image>
+        </view>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        }
+    }
 </script>
 
 <style>
-	.box1{
-		width: 100%;
-		height: 120px;
-		background: #FFFFFF;
-		display: flex;
-		
-		
-		
-	}
-	.sandian{
-		width: 27px;
-		height: 27px;
-		padding-left: 269px;
-		padding-top: 70px;
-	}
-	.biaoti{
-		font-size: 55rpx;
-		font-weight: 700;
-		padding-left: 25px;
-		padding-top: 60px;
-		padding-bottom:25px;
-	}
+    .box1{
+        width: 100%;
+        height: 120px;
+        background: #FFFFFF;
+        display: flex;
+        
+    }
+    .sandian{
+        width: 27px;
+        height: 27px;
+        padding-left: 60%;
+        padding-top: 17%;
+    }
+    .biaoti{
+        font-size: 55rpx;
+        font-weight: 700;
+        padding-left: 10%;
+        padding-top: 15%;
+        padding-bottom:25px;
+		white-space: nowrap;
+    }
 </style>
