efcbc1d19cb4b00d251d0e2b1d3f493ce5fdba31
zaohuan <1870471060@qq.com>
2024-10-09 21:16:35 +0800

    Update projectdetailfenlei.vue

--- 软工app/pages/projectdetailfenlei/projectdetailfenlei.vue
+++ 软工app/pages/projectdetailfenlei/projectdetailfenlei.vue
@@ -6,8 +6,8 @@
 		  </view>
 		  <text class="biaoti2">项目详情</text>
 		</view>
-		
-		<view class="ddetails" v-if="projectData">
+		<view v-if="loading" class="loading">加载中...</view>
+		<view class="ddetails" v-if="l1">
 			<text>项目名称：{{ projectData.name }}</text>
 			<text>项目简介：{{ projectData.description }}</text>
 			<text>项目分类：{{ projectData.category }}</text>
@@ -20,9 +20,8 @@
 			<text>创建人所在学院：{{academy}}</text>
 			<text>创建人身份：{{identity}}</text>
 		</view>
-		<view v-else>
-			<text>没有项目数据</text>
-		</view>
+		
+		
 	</view>	
 			
 			
@@ -38,7 +37,8 @@ export default {
   data() {
     return {
       projectData: null,
-	  username:'',
+	  loading: false, // 加载状态
+	  l1:false,
 	  realname: '',
 	  identity:'',
 	  academy:'',
@@ -52,28 +52,33 @@ export default {
       key: 'projectData',
       success: (res) => {
         this.projectData = res.data; // 将数据存储到项目数据中
+		this.getUserData();
+		
       },
       fail: () => {
         console.log('没有找到项目数据');
       }
     });
-	this.getUserData();
+	
+	
 	
   },
   methods:{
+	  
 	  goBack() {
 	    uni.navigateBack({
 	      delta: 1
 	    });
 	  },
 	  async getUserData() {
+		this.loading = true;
+		this.l1=false;
 	    try {
-	  	  
 	      const res = await uniCloud.callFunction({
 	        name: 'getdetailbyusername',
 	        data: {
-	          username: this.projectData.username 
-	        }
+	          username: this.projectData.username
+			  }
 	      });
 	  
 	      if (res.result.code === 200 && res.result.data) {
@@ -86,7 +91,10 @@ export default {
 	      }
 	    } catch (error) {
 	      console.error('调用云函数失败', error);
-	    }
+	    }finally{
+			this.loading = false;  // 完成加载
+			this.l1=true;
+		}
 	  },
   },
   
@@ -132,18 +140,7 @@ export default {
 	    font-size: 35rpx;
 	    color: #000000;
 	}
-	.box2{
-	    width: 100%;
-	    height: 80px;
-	    background: #FFFFFF;
-	    display: flex;
-	    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-	    justify-content: center;
-	    align-items: center;
-	    position: fixed;
-		top: 0;
-		z-index: 1000;
-	}
+
 	.box3{
 	    width: 100%;
 	    height: 80px;
@@ -156,5 +153,14 @@ export default {
 	.biaoti2 {
 	  font-size: 55rpx;
 	  font-weight: 700;
+	}
+	.loading {
+	    display: flex;
+	    justify-content: center;
+	    align-items: center;
+	    height: 500rpx; 
+	    font-size: 50rpx;
+	    color: #999;
+		
 	}
 </style>
